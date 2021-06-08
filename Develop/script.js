752760cf1e5d94db9b00830bb5170b0b84b65b40
Spenny Berks <sberkebile@live.com>
2021-06-08 09:55:49 -0400

    added date to header and created var array for hours

---
+++ Develop/script.js
@@ -0,0 +1,102 @@
+//Creates array for each hour of today
+var day = [
+    {
+        id: "0",
+        hour: "6",
+        time: "6",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "7",
+        time: "7",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "8",
+        time: "8",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "9",
+        time: "9",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "10",
+        time: "10",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "11",
+        time: "11",
+        merdiem: "am",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "12",
+        time: "12",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "1",
+        time: "13",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "2",
+        time: "14",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "3",
+        time: "15",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "4",
+        time: "16",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "5",
+        time: "17",
+        merdiem: "pm",
+        schedule: ""
+    },
+    {
+        id: "0",
+        hour: "6",
+        time: "18",
+        merdiem: "pm",
+        schedule: ""
+    },
+]
+
+//Uses moment.js to  find today's date
+function todayDate() {
+    var currentDay = moment().format("MMM Do YY");
+    $("#currentDay").text(currentDay);
+}
+
+todayDate();
