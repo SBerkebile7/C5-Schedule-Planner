21529bb8052cbafb41e8a2cb1d9c7eae72b67e1d
Spenny Berks <sberkebile@live.com>
2021-06-08 13:40:24 -0400

    working on fix for .forEach failue

--- Develop/script.js
+++ Develop/script.js
@@ -8,84 +8,84 @@ var day = [
         schedule: ""
     },
     {
-        id: "0",
+        id: "1",
         hour: "7",
         time: "7",
         merdiem: "am",
         schedule: ""
     },
     {
-        id: "0",
+        id: "2",
         hour: "8",
         time: "8",
         merdiem: "am",
         schedule: ""
     },
     {
-        id: "0",
+        id: "3",
         hour: "9",
         time: "9",
         merdiem: "am",
         schedule: ""
     },
     {
-        id: "0",
+        id: "4",
         hour: "10",
         time: "10",
         merdiem: "am",
         schedule: ""
     },
     {
-        id: "0",
+        id: "5",
         hour: "11",
         time: "11",
         merdiem: "am",
         schedule: ""
     },
     {
-        id: "0",
+        id: "6",
         hour: "12",
         time: "12",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "7",
         hour: "1",
         time: "13",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "8",
         hour: "2",
         time: "14",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "9",
         hour: "3",
         time: "15",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "10",
         hour: "4",
         time: "16",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "11",
         hour: "5",
         time: "17",
         merdiem: "pm",
         schedule: ""
     },
     {
-        id: "0",
+        id: "12",
         hour: "6",
         time: "18",
         merdiem: "pm",
@@ -101,26 +101,28 @@ function todayDate() {
 
 todayDate();
 
-// Saves reminders
-function saveReminders() {
-    localStorage.setItem("day", JSON.stringify("day"));
+// Saves schedule reminders
+function saveSchedule() {
+    localStorage.setItem("day", JSON.stringify(day));
 }
 
-// Displays reminders 
-function displayReminders() {
-    
+// Displays schedule reminders 
+function displaySchedule() {
+    day.forEach(function(_hourTime) {
+        $(`#${_hourTime.id}`).val(_hourTime.schedule)
+    })
 }
 
-// Checks and applies and saved reminders upon page load
-function loadReminders () {
-    var storedReminder = JSON.parse(localStorage.getItem("day"));
+// Checks and applies and saved schedule reminders upon page load
+function loadSchedule () {
+    var storedSchedule = JSON.parse(localStorage.getItem("day"));
 
-    if(storedReminder) {
-        day = storedReminder;
+    if(storedSchedule) {
+        day = storedSchedule;
     }
 
-    saveReminders();
-    displayReminders();
+    saveSchedule();
+    displaySchedule();
 }
 
 // Displays blocks of time as hours
@@ -132,16 +134,51 @@ day.forEach(function(currentHour) {
     $(".container").append(timeBlock);
 
     // Creates the hour block of the time row
-    var hourTime;
+    var hourTime = $("<div>")
+    .text(`${currentHour.hour}${currentHour.meridiem}`)
+    .attr({
+        "class": "col-md-1 hour"
+    });
 
     // Creates the reminder block of the time row
-    var hourReminder;
+    var hourReminder = $("<div>")
+    .attr({
+        "class": "col-md-9 description p-0"
+    });
+    
+    // Adjusts the time row to reflect past, present or future
+    var timeColorAdjust = $("<textarea>");
+
+    hourReminder.append(timeColorAdjust);
+    timeColorAdjust.attr("id", currentHour.id);
+
+    // Checks current time and assigns class based on past/present/future to row
+    if(currentHour.time < moment().format("HH")) {
+        timeColorAdjust.attr ({
+            "class" : "past"
+        })
+    } else if(currentHour.time === moment().format("HH")) {
+        timeColorAdjust.attr ({
+            "class" : "present"
+        })
+    } else if(currentHour.time > moment().format("HH")) {
+        timeColorAdjust.attr ({
+            "class" : "future"
+        })
+    }
 
     // Creates the save block of the time row
-    var hourSave;
+    var hourSaveBtn = $("<i class='far fa-solid fa-floppy-disks fa-lg'</i>");
+    var hourSave = $("<button>")
+    .attr({
+        "class" : "col-md-1 saveBtn"
+    });
+
+    hourSave.append(hourSaveBtn)
+    timeBlock.append(hourTime, hourReminder, hourSave);
 })
 
-loadReminders();
+loadSchedule();
 
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
